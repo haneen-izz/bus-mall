2ebef36ac6edbc299729d130dbc6cfbbb7e291d3
haneen <haneenjaradeh1@gmail.com>
2021-08-16 14:12:27 +0300

    changes2

--- app.js
+++ app.js
@@ -1,93 +1,136 @@
+
 'use strict';
+
 let attemptEl = document.getElementById('attempts');
-let container = document.getElementById('image-container');
+let container = document.getElementById('container');
 let leftImg = document.getElementById('leftImg');
 let middleImg = document.getElementById('middleImg');
 let rightImg = document.getElementById('rightImg');
 let result = document.getElementById('results');
 
-let productsImg = ['bag.jpg','banana.jpg', 'bathroom.jpg','boots.jpg', 'breakfast.jpg','bubblegum.jpg', 'chair.jpg' ,'cthulhu.jpg' ,'dog-duck.jpg', 'dragon.jpg' ,'pen.jpg', 'pet-sweep.jpg','scissors.jpg', 'shark.jpg','sweep.png','tauntaun.jpg ', 'unicorn.jpg',
-'water-can.jpg ','wine-glass.jpg'  ];
-let products= [];
-let attempt = 1;
+
+
+let pImage = ['bag.jpg', 'banana.jpg', 'breakfast.jpg', 'bathroom.jpg', 'boots.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg ', 'unicorn.jpg', 'water-can.jpg ', 'wine-glass.jpg'];
+
+
 let maxAttempt = 25;
+let productArray = [];
+let attempt = 1;
 
+function ProductImag(productName) {
 
-function ProductImg(productName){
-this.productName = productName.split('')[0];
-this.productImg = `images/${productName}`;
-this.votes = 0 ;
-this.views = 0;
-products.push(this);
-}
-for(let i = 0 ; i < productsImg.length; i++){
-new ProductImg(productsImg[i]);
+    this.productName = productName.split('.')[0];
+    this.productImage = `images/${productName}`;
+    this.votes = 0;
+    this.views = 0;
 
-}
-function randomProduct(){
-return Math.floor(Math.random()*products.length);
-}
-
-let leftIndex;
-let middleIndex;
-let rightIndex;
-function renderProduct(){
-leftImg =  randomProduct();
-middleImg = randomProduct();
-rightImg = randomProduct();
-
-while(leftIndex === rightIndex){
-leftIndex = randomProduct();
-}
-while(leftIndex === middleIndex) {
-leftIndex = randomProduct();
+    productArray.push(this);
 
 }
-while(middleIndex === rightIndex) {
-    leftIndex = randomProduct();
+    for (let i = 0; i < pImage.length; i++) {
+        new ProductImag(pImage[i]);
+    }
     
-}
-leftImg.setAttribute("src" , products[leftIndex].leftImg);
-middleImg.setAttribute("src", products[middleIndex].middleImg );
-rightImg.setAttribute("src" , products[rightIndex].rightImg);
+    
+    function randomProduct() {
+        return Math.floor(Math.random() * productArray.length);
+    
+    }
+    
 
-products[leftIndex].views++;
-products[middleIndex].views++;
-products[rightIndex].views++;
-}
-renderProduct();
-leftImg.addEventListener('click', clickHandler );
-middleImg.addEventListener('click', clickHandler);
-rightImg.addEventListener('click', clickHandler);
+    let leftIndex;
+    let middleIndex;
+    let rightIndex;
+    
+    
+    
+    function renderProduct() {
+        rightIndex = randomProduct();
+        middleIndex = randomProduct();
+        leftIndex = randomProduct();
+      
+    
+    
+    
+        while (leftIndex === middleIndex || middleIndex === rightIndex || rightIndex === leftIndex) {
+    
+            leftIndex = randomProduct();
+            rightIndex = randomProduct();
+    
+        }
+    
+    
+        rightImg.setAttribute('src', productArray[rightIndex].productImage);
+        middleImg.setAttribute('src', productArray[middleIndex].productImage);
+        leftImg.setAttribute('src', productArray[leftIndex].productImage);
+    
 
-function clickHandler(event){
-if(attempt <= maxAttempt){
-let clickedImg = event.target.middleImg;
-if(clickedImg === 'leftImg'){
-products[leftIndex].votes++;
-}
-else if(clickedImg === 'middleImg'){
-products[middleIndex].votes++
-}
-else if(clickedImg === 'rightImg'){
-products[rightIndex].votes++
-}
-renderImg();
-attempt++;
-
-}else {
-leftImg.removeEventListener('click', clickHandler );
-middleImg.removeEventListener('click', clickHandler);
-rightImg.removeEventListener('click', clickHandler);
-}
-}
-function getResult(){
-for(let i = 0 ; i< products.length ;i++){
-let liEl = document.createElement('li');
-result.appendChild(liEl);
-liEl.textContent = `${products[i].productName} has ${products[i].votes} votes and  ${products[i].views} views.`;
-}
-}
+    
+        productArray[leftIndex].views++;
+        productArray[middleIndex].views++;
+        productArray[rightIndex].views++;
+    
+    }
+    renderProduct (); 
+    
+    
+    leftImg.addEventListener('click', clickHandler);
+    middleImg.addEventListener('click', clickHandler);
+    rightImg.addEventListener('click', clickHandler);
+    
+    
+    
+    function clickHandler(event) {
+    
+        if (attempt <= maxAttempt) {
+    
+            let clickedImg = event.target.id;
+            if (clickedImg === 'rightImg') {
+                productArray[leftIndex].votes++;
+    
+            }
+    
+            else if (clickedImg === 'middleImg') {
+                productArray[middleIndex].votes++
+    
+            }
+    
+    
+    
+            else if (clickedImg === 'leftImg' ) {
+                productArray[rightIndex].votes++
+    
+            }
+    
+    
+    
+            renderProduct();
+            attempt++;
+    
+    
+    
+        }
+    
+    }
+    
+    
+    
+    let button1 = document.getElementById('button');
+    button1.addEventListener('click', showResult);
+    
+    
+    
+    function showResult() {
+        for (let i = 0; i < productArray.length; i++) {
+            let liEl = document.createElement('li');
+            result.appendChild(liEl)
+            liEl.textContent = `${productArray[i].productName} has ${productArray[i].votes} votes and  ${productArray[i].views} views.`;
+    
+    
+    
+        }
+    }
+    
 
 
 
